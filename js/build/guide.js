9e8350c999e0c83a3a8c066b155a75b84fce71b9
Ryan <rburgess@evernote.com>
2014-06-30 20:30:05 -0700

    a videos to the guide list output

--- js/build/guide.js
+++ js/build/guide.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function($) {
     'use strict';
     var html;
@@ -116,3 +117,78 @@
     });
     
 }(jQuery));
+},{}],2:[function(require,module,exports){
+(function($) {
+    'use strict';
+    
+        //load modal mask
+        $('<div id="mask"></div>').appendTo('body');
+
+        //modal windows
+        $('body').on('click', '.modal', function(e) {
+            //remove existing modal window
+            $('#modal-window').remove();
+
+            var content;
+
+            //Checks if href points to media or a div that is defined in the DOM
+            if ($(this).attr('href').match('^#')) {
+                var contentID = $(this).attr('href');
+                content = $(contentID).html();
+            } else {
+                content = $(this).attr('href');
+            }
+
+            var fullContent = content;
+            var vidParameters = '?vq=hd720&autoplay=1&showinfo=0&rel=0';
+            //if youtube video or images passed into modal
+            if(content.indexOf('youtube.com/embed/') !== -1){
+                fullContent = '<iframe src="'+ content + vidParameters +'" allowfullscreen="allowfullscreen"></iframe>';
+            }else if(content.indexOf('jpg') !== -1 || content.indexOf('png') !== -1){
+                fullContent = '<img src="'+ content +'">';
+            }
+
+            //load modal window with content passed
+            $('<div id="modal-window" class="window">'+ fullContent +'<a href="#" class="close">X</a></div>').appendTo('body');
+
+            var maskHeight = $(document).height();
+            var maskWidth = $(window).width();
+
+            $('#mask').css({'width':maskWidth,'height':maskHeight});
+
+            $('#mask').show();
+
+            $('.window').css('top', ( $(window).height() - $('.window').height() ) / 2+$(window).scrollTop() + 'px');
+            $('.window').css('left', ( $(window).width() - $('.window').width() ) / 2+$(window).scrollLeft() + 'px');
+
+            $('.window').fadeIn();
+
+            e.preventDefault();
+            return false;
+        });
+
+        //if close button is clicked
+        $('body').on('click', '.window .close', function (e) {
+            //Cancel the link behavior
+            e.preventDefault();
+            $('#mask, .window').fadeOut();
+            $('.window iframe').remove();
+        });
+
+        //if mask is clicked
+        $('#mask').on('click', function (e) {
+            //Cancel the link behavior
+            e.preventDefault();
+            $('#mask, .window').hide();
+        });
+
+        // Change modal window size repsonsive
+        function checkWindowSize(e) {
+            $('.window').css('top', ( $(window).height() - $('.window').height() ) / 2+$(window).scrollTop() + 'px');
+            $('.window').css('left', ( $(window).width() - $('.window').width() ) / 2+$(window).scrollLeft() + 'px');
+            $('#mask').css({'width':$(window).width(),'height':$(document).height()});
+        }
+        $(window).bind('resize', checkWindowSize);
+
+}(jQuery));
+},{}]},{},[1,2])
